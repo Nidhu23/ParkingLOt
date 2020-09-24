296ff6edf814e1803862ea0b7fc6ba0e1a134ae0
nidhu23 <nidhunanik@gmail.com>
2020-09-24 22:56:26 +0530

    [Nidhuna]Add: url pattern for login

--- Users/urls.py
+++ Users/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
 from Users import views
 
-urlpatterns = [
-    path('register/', views.register),
-]
+urlpatterns = [path('register/', views.register), path('login/', views.login)]
