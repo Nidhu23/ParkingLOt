75d2d80cd173dda07a772333ee773376f81dca5e
nidhu23 <nidhunanik@gmail.com>
2020-09-24 17:58:13 +0530

    [Nidhuna]Add: connection config file for redis

---
+++ Users/redis_setup.py
@@ -0,0 +1,8 @@
+import redis
+from ParkingLotSystem import settings
+
+
+def get_redis_instance():
+    return redis.Redis(host=settings.REDIS_HOST,
+                       port=settings.REDIS_PORT,
+                       db=0)
