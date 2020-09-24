dc85145aa0872a6e231eef4d3d1be25ae0371bfe
nidhu23 <nidhunanik@gmail.com>
2020-09-25 00:29:44 +0530

    [Nidhuna]Add: url pattern for logout

--- Users/urls.py
+++ Users/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 from Users import views
 
-urlpatterns = [path('register/', views.register), path('login/', views.login)]
+urlpatterns = [
+    path('register/', views.register),
+    path('login/', views.login),
+    path('logout/', views.logout)
+]
