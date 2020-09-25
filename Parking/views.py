0e7a3b8ff88332f3aec7a71feda9abe037ec96af
nidhu23 <nidhunanik@gmail.com>
2020-09-25 11:27:52 +0530

    [Nidhuna]Add: Api's to park and unpark a vehicle

---
+++ Parking/views.py
@@ -0,0 +1,68 @@
+from django.shortcuts import render
+from django.shortcuts import render
+from rest_framework import generics, mixins, permissions
+from rest_framework.decorators import api_view
+from .serializers import ParkSerializer
+from .models import Parking, UnPark
+from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.exceptions import ParseError
+from rest_framework.response import Response
+import logging
+from .decorators import jwt_decode, role_required
+from . import services
+logger = logging.getLogger('django')
+# Create your views here.
+
+
+@api_view(['POST'])
+@jwt_decode
+def park(request):
+    ''' 
+    API that lets users park vehicles
+
+    Parameters:
+    request paramter: having vehicle details
+
+    Returns:
+    parks the vehicle and returns status code
+    '''
+    try:
+        slot_list = set(Parking.objects.values_list("slot", flat=True))
+        slot = services.assign_slot(slot_list)
+        set_slot = Parking()
+        setattr(set_slot, 'slot', slot)
+        serializer = ParkSerializer(data=request.data, instance=set_slot)
+        if serializer.is_valid():
+            serializer.save()
+            return Response("Your car has been parked",
+                            status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    except Parking.DoesNotExist:
+        logger.error("parking object not found")
+        return Response("Park object does not exist",
+                        status=status.HTTP_404_NOT_FOUND)
+    except ParseError:
+        return Response("Check your request data",
+                        status=status.HTTP_400_BAD_REQUEST)
+    except Exception as exception:
+        logger.error(exception)
+        return Response("Vehicle could not be parked",
+                        status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+@api_view(['DELETE'])
+@jwt_decode
+def unpark(request):
+    try:
+        vehicle_num = request.data.get('vehicle_num')
+        park_details = Parking.objects.get(vehicle_num=vehicle_num)
+        services.un_park(park_details)
+        return Response("Unparked", status=status.HTTP_201_CREATED)
+    except Parking.DoesNotExist as exception:
+        logger.error("parking object not found", exception)
+        return Response("The vehicle with this number is not parked here",
+                        status=status.HTTP_404_NOT_FOUND)
+    except Exception as ex:
+        logger.error("unpark exception", ex)
+        return Response(ex, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
