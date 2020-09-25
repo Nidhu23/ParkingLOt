000f2a100601139e33b52e030acd2b161c432900
nidhu23 <nidhunanik@gmail.com>
2020-09-25 13:11:43 +0530

    [Nidhuna]Add: url patterns for search vehicle api

--- Parking/urls.py
+++ Parking/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 from Parking import views
 
-urlpatterns = [path('park/', views.park), path('unpark/', views.unpark)]
+urlpatterns = [
+    path('park/', views.park),
+    path('unpark/', views.unpark),
+    path('searchbynum/', views.vehicle_num_search)
+]
